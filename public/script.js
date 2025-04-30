84de8400652d1c8057c8a1f8b8239f0c17b902e8
yuvaraj <ajayarun177@gmail.com>
2025-04-30 14:00:30 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,17 @@
+const socket = io();
+const input = document.getElementById('messageInput');
+const messages = document.getElementById('messages');
+
+function sendMessage() {
+  const msg = input.value.trim();
+  if (msg) {
+    socket.emit('chat message', msg);
+    input.value = '';
+  }
+}
+
+socket.on('chat message', function(msg) {
+  const li = document.createElement('li');
+  li.textContent = msg;
+  messages.appendChild(li);
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
